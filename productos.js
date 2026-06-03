f6041df69ad2398d2b208050ff54a14e7431d499
sabinocatrielmarquezcarrasco-hash <sabinocatrielmarquezcarrasco@gmail.com>
2026-06-03 12:32:39 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,5 +1,4 @@
-// productos.js
-export const PRODUCTOS_DATA = [
+const PRODUCTOS_DATA = [
     {
         "id": "BF-888S",
         "name": "HANDY  X 2  BAOFENG  5W",
@@ -25,28 +24,19 @@ export const PRODUCTOS_DATA = [
         "image": "https://images.unsplash.com/photo-1581244277943-fe4a9c777189?w=300&q=80"
     },
     {
+        "id": "1903",
+        "name": "CINTA METRICA GIANT  7.5 METROS",
+        "price": 2999,
+        "category": "HERRAMIENTAS",
+        "pack": "120",
+        "image": "https://images.unsplash.com/photo-1581244277943-fe4a9c777189?w=300&q=80"
+    },
+    {
         "id": "LED-9726",
         "name": "LED-83094 TIRA LED 50X50 RGB 5M 9734",
         "price": 6700,
         "category": "ILUMINACION",
         "pack": "40",
         "image": "https://images.unsplash.com/photo-1565814636199-ae8133055c1c?w=300&q=80"
-    },
-    {
-        "id": "AR-1401",
-        "name": "VINCHA  P47  BT MELECH-232 16009",
-        "price": 3250,
-        "category": "AURICULARES VINCHA O BT",
-        "pack": "100",
-        "image": "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=300&q=80"
-    },
-    {
-        "id": "GTS-1802",
-        "name": "(BS PARLANTE BT 8 C/LUZ MIC",
-        "price": 24500,
-        "category": "PARLANTES Y MICROFONOS",
-        "pack": "8",
-        "image": "https://images.unsplash.com/photo-1608043152269-423dbba4e7e1?w=300&q=80"
     }
-    // ... El script procesó los 403 artículos vinculando los códigos bulto automáticamente.
 ];
